3abbc982800ed96a6b5ea5f32179acf32db20d7a
Sevrinn <sevrinnsevrinn@gmail.com>
2022-02-14 10:01:02 -0800

    star rating with useEffect

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+import { FaStar } from 'react-icons/fa'
+
 import reportWebVitals from './reportWebVitals'
+
+const createArray = (length) => [...Array(length)]
+
+const Star = ({ selected = false, onSelect }) => {
+  return <FaStar color={selected ? 'red' : 'gray'} onClick={onSelect} />
+}
+
+const StarRating = ({ totalStars = 5 }) => {
+  const [selectedStars, setSelectedStars] = useState(0)
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+      <p>
+        {selectedStars} of {totalStars}
+      </p>
+    </>
+  )
+}
+
+const App = () => {
+  const [checked, setChecked] = useState(false)
+  return <StarRating totalStars={4} />
+}
 
 ReactDOM.render(
   <React.StrictMode>
